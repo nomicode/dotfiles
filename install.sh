9a5b8a432b87a01b57981dce0d660f8658a67297
Naomi Rose <23469+nomirose@users.noreply.github.com>
2022-12-19 18:10:11 +0000

    feat: wip ssh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -e
 
 rsync -av --copy-links home/ "${HOME}"
+
+if test -n "${SSH_PRIVATE_KEY}"; then
+    mkdir -p "${HOME}/.ssh"
+    echo "${SSH_PRIVATE_KEY}" > "${HOME}/.ssh/id_rsa"
+    chmod 600 "${HOME}/.ssh/id_rsa"
+fi
