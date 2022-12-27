512957c5f96602c826d4b029064dd580d88d9654
Naomi Rose <23469+nomirose@users.noreply.github.com>
2022-12-27 01:18:15 +0100

    feat: clone the dotfiles repository in home dir (#22)
    - Use repository secrets to install SSH keys for commit signing and
    repository authentication
    - Add a `known_hosts` file with GitHub fingerprints
    - Clone the dotfiles repository into the home directory
    - Set up `ssh-agent` and `direnv` programs with a custom `.profile` file
    - Add placeholder files for future development
    - Configure file installations with a `manifest.txt` file

---
+++ home/.profile
@@ -0,0 +1,24 @@
+# Default code
+# =============================================================================
+# ~/.profile: executed by Bourne-compatible login shells.
+
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+
+mesg n 2> /dev/null || true
+
+# Custom code
+# =============================================================================
+
+eval "$(ssh-agent bash)"
+
+SSH_KEY_FILE="${HOME}/.ssh/id_rsa"
+if test -s "${SSH_KEY_FILE}"; then
+    ssh-add "${SSH_KEY_FILE}"
+fi
+unset SSH_KEY_FILE
+
+eval "$(direnv hook bash)"
--- install.sh
+++ install.sh
@@ -1,5 +1,70 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
-rsync -av --copy-links home/ "${HOME}"
+# Install manifest files into the home directory
+# =============================================================================
 
-git config --global push.default current
+install_files() {
+    source_dir="${1}"
+    while read -r file; do
+        install --compare -Dv "${source_dir}/${file}" "${HOME}"
+    done <manifest.txt
+}
+
+install_files "$(pwd)"
+
+# SSH keys
+# =============================================================================
+
+SSH_DIR="${HOME}/.ssh"
+SSH_PUBKEY_FILE="${SSH_DIR}/id_rsa.pub"
+SSH_KEY_FILE="${SSH_DIR}/id_rsa"
+
+create_ssh_keys() {
+    # Execute in a subshell and discard all output
+    (
+        # Exit if SSH key files already exist
+        if test -s "${SSH_PUBKEY_FILE}" -o -s "${SSH_KEY_FILE}"; then
+            exit 0
+        fi
+        # Write out env variables
+        echo "${VSCODE_PUBLIC_KEY:=}" >"${SSH_PUBKEY_FILE}"
+        echo "${VSCODE_PRIVATE_KEY:=}" >"${SSH_KEY_FILE}"
+        # Check file sizes as a proxy for testing if variables were set
+        if test -s "${SSH_PUBKEY_FILE}" -a -s "${SSH_KEY_FILE}"; then
+            # Configure SSH key files and exit
+            chmod 640 "${SSH_PUBKEY_FILE}"
+            chmod 600 "${SSH_KEY_FILE}"
+            exit 0
+        fi
+        # Clean up empty files
+        rm -f "${SSH_PUBKEY_FILE}"
+        rm -f "${SSH_KEY_FILE}"
+        rmdir --ignore-fail-on-non-empty "${SSH_DIR}"
+    ) >/dev/null 1>&2
+}
+
+create_ssh_keys
+
+# Git
+# =============================================================================
+
+modify_git_conf() {
+    git config --global commit.gpgsign true
+    git config --global gpg.format ssh
+    git config --global user.signingkey "${SSH_PUBKEY_FILE}"
+}
+
+modify_git_conf
+
+DOTFILES_GIT="git@github.com:nomirose/dotfiles.git"
+DOTFILES_DIR="${HOME}/dotfiles"
+
+clone_dotfiles() {
+    if ! test -d "${DOTFILES_DIR}"; then
+        if test "$(pwd)" != "${DOTFILES_DIR}"; then
+            git clone "${DOTFILES_GIT}" "${DOTFILES_DIR}"
+        fi
+    fi
+}
+
+clone_dotfiles
