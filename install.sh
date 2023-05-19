27829fc196992361b936a2e4d08dcc1f1eb5f04e
Naomi Rose <23469+nomicode@users.noreply.github.com>
2023-05-19 11:14:37 +0200

    fix: `create_ssh_keys` function bugs (#31, #32, #33) (#34)

--- install.sh
+++ install.sh
@@ -26,20 +26,30 @@ create_ssh_keys() {
         if test -s "${SSH_PUBKEY_FILE}" -o -s "${SSH_KEY_FILE}"; then
             exit 0
         fi
-        # Write out env variables
-        echo "${VSCODE_PUBLIC_KEY:=}" >"${SSH_PUBKEY_FILE}"
-        echo "${VSCODE_PRIVATE_KEY:=}" >"${SSH_KEY_FILE}"
-        # Check file sizes as a proxy for testing if variables were set
-        if test -s "${SSH_PUBKEY_FILE}" -a -s "${SSH_KEY_FILE}"; then
-            # Configure SSH key files and exit
-            chmod 640 "${SSH_PUBKEY_FILE}"
-            chmod 600 "${SSH_KEY_FILE}"
-            exit 0
-        fi
-        # Clean up empty files
-        rm -f "${SSH_PUBKEY_FILE}"
-        rm -f "${SSH_KEY_FILE}"
-        rmdir --ignore-fail-on-non-empty "${SSH_DIR}"
+        mkdir -p "${SSH_DIR}"
+        # Write out the SSH keys (using environment variables set by GitHub
+        # from the repository secrets)
+        (
+            # Don't print secrets to stdout when this script run with the `-x`
+            # argument
+            set +x
+            echo "Temporarily disabled command output to write secrets..."
+            write_file() {
+                perms="${1}"
+                filename="${2}"
+                env_var="${3}"
+                secret="$(eval "echo \"\${${env_var}:=}\"")"
+                if test -z "${secret}"; then
+                    echo "Error: \`${env_var}\` is empty" >&2
+                    exit 1
+                fi
+                echo "Writing \`${env_var}\` to the \`${filename}\` file..."
+                echo "${secret}" >"${filename}"
+                chmod "${perms}" "${filename}"
+            }
+            write_file 640 "${SSH_PUBKEY_FILE}" VSCODE_PUBLIC_KEY
+            write_file 600 "${SSH_KEY_FILE}" VSCODE_PRIVATE_KEY
+        )
     ) >/dev/null 1>&2
 }
 
@@ -56,7 +66,7 @@ modify_git_conf() {
 
 modify_git_conf
 
-DOTFILES_GIT="git@github.com:nomirose/dotfiles.git"
+DOTFILES_GIT="https://github.com/nomicode/dotfiles.git"
 DOTFILES_DIR="${HOME}/dotfiles"
 
 clone_dotfiles() {
